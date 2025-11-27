3700b69bc4fa04326ee3f3f9f2726ea8407e6d4c
miguel-octavio-aguila <migueloctavioaguila1@gmail.com>
2025-11-27 03:50:12 -0600

    feat: Add core URL configuration for admin and static files.

--- 1_Blog/backend/core/urls.py
+++ 1_Blog/backend/core/urls.py
@@ -20,9 +20,6 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import TestView
-
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("test/", TestView.as_view(), name="test"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
