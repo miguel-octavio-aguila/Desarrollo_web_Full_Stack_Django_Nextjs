a10ec20c5c319d71bfe29156939353149fe3bd01
miguel-octavio-aguila <migueloctavioaguila1@gmail.com>
2025-12-07 02:47:11 -0600

    feat: Implement initial blog application models, core URL configuration, and dependencies.

--- 1_Blog/backend/apps/blog/models.py
+++ 1_Blog/backend/apps/blog/models.py
@@ -1,9 +1,32 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.text import slugify
+
+import uuid
 
 # This function is used to store the thumbnail in a specific directory
 def blog_thumbnail_directory(instance, filename):
     return "blog/{0}/{1}".format(instance.title, filename)
+
+# This function is used to store the thumbnail in a specific directory
+def category_thumbnail_directory(instance, filename):
+    return "blog_categories/{0}/{1}".format(instance.name, filename)
+
+class Category(models.Model):
+    # This field is used to create a unique identifier for the category
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    
+    # This field is used to create a tree of categories
+    parent = models.ForeignKey("self", related_name="children", on_delete=models.CASCADE, blank=True, null=True)
+    
+    name = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField()
+    thumbnail = models.ImageField(upload_to=category_thumbnail_directory)
+    slug = models.CharField(max_length=128)
+    
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     # Manager for published posts
@@ -17,24 +40,57 @@ class Post(models.Model):
         ("published", "Published"),
     )
     
-    title = models.CharField(_("Title"), max_length=128)
-    description = models.CharField(_("Description"), max_length=256)
-    content = models.TextField(_("Content"))
-    thumbnail = models.ImageField(_("Thumbnail"), upload_to=blog_thumbnail_directory)
+    # This field is used to create a unique identifier for the post
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     
-    keywords = models.CharField(_("Keywords"), max_length=128)
-    slug = models.CharField(_("Slug"), max_length=128)
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=256)
+    content = models.TextField()
+    thumbnail = models.ImageField(upload_to=blog_thumbnail_directory)
     
-    created_at = models.DateTimeField(_("Created at"), default=timezone.now)
-    updated_at = models.DateTimeField(_("Updated at"), auto_now=True)
+    keywords = models.CharField(max_length=128)
+    slug = models.CharField(max_length=128)
     
-    status = models.CharField(_("Status"), max_length=10, choices=status_options, default="draft")
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    status = models.CharField(max_length=10, choices=status_options, default="draft")
     
     objects = models.Manager() # Default manager
     published = PostObject() # Manager for published posts (custom manager)
     
     class Meta:
-        ordering = ("-published")
-        
+        ordering = ("status", "-created_at")
+
     def __str__(self):
         return self.title
+    
+class Heading(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    
+    post = models.ForeignKey(Post, on_delete=models.PROTECT, related_name="headings")
+    
+    title = models.CharField(max_length=128)
+    slug = models.CharField(max_length=128)
+    level = models.IntegerField(
+        choices=(
+            (1, "H1"),
+            (2, "H2"),
+            (3, "H3"),
+            (4, "H4"),
+            (5, "H5"),
+            (6, "H6"),
+        )
+    )
+    order = models.PositiveIntegerField()
+    
+    class Meta:
+        ordering = ["order"]
+        
+    # This method is used to create a slug for the heading
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
