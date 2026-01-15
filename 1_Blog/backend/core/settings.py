b784bdc689a77d3c8e6145f5913a3c6642596839
miguel-octavio-aguila <migueloctavioaguila1@gmail.com>
2026-01-15 12:30:46 -0600

    feat: Initialize Django project with Docker Compose, PostgreSQL, Redis, Celery, Channels, DRF, CKEditor, and AWS S3 storage.

--- 1_Blog/backend/core/settings.py
+++ 1_Blog/backend/core/settings.py
@@ -197,9 +197,9 @@ USE_TZ = True
 # STATIC_LOCATION = "static"
 # STATIC_URL = "static/"
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static_local"),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static_local"),
+]
 # MEDIA_URL = "media/"
 # MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
